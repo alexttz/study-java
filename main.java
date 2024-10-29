bb8da761b059761c3a309aeb594e31756b179c48
Alex Lima <lima.alexander.las@gmail.com>
2024-10-29 11:15:00 -0300

    First commit

---
+++ main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import javax.swing.JFrame;
+
+
+public class main {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame();
+        janela.setTitle("Meu primeiro programa");
+        janela.setSize(500,500);
+        janela.setVisible(true);
+    }
+}
