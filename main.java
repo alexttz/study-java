026bc89e42f4dedb1099890b94a5ccac4bdd17bb
Alex Lima <lima.alexander.las@gmail.com>
2024-10-30 14:07:58 -0300

    Add button on JFrame and create alert

--- main.java
+++ main.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 public class main {
     public static void main(String[] args) {
@@ -10,15 +12,25 @@ public class main {
         janela.setSize(500,500);
         janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //Metodo que encerra o programa quando clicamos no "X" na janela.
 
-        JLabel labelNome = new JLabel("Nome:"); // Criando uma label, inserindo um texto na janela
-
         janela.setLayout(null); // removendo layouts da janela que vem por padrão
-        janela.getContentPane().add(labelNome); // adicionando a label na janela criada
-        labelNome.setBounds(20,20,50,20); // configurando a posião do texto (label)
 
+        //Criações e ajustes da tela
+        JLabel labelNome = new JLabel("Nome:"); // Criando uma label, texto e botão, inserindo um texto na janela
         JTextField campoTextoNome = new JTextField();
+        JButton botao = new JButton("Pressione");
+
         campoTextoNome.setBounds(20,40,200,20);
+        labelNome.setBounds(20,20,50,20); // configurando a posição dos elementos
+        botao.setBounds(20,60,100,20);
+
+        botao.addActionListener(event -> { //Criando um evento ao pressionar o botão
+            String nome = campoTextoNome.getText();
+            JOptionPane.showMessageDialog(janela, "Olá " + nome);
+        });
+
+        janela.getContentPane().add(labelNome); // adicionando os elementos na janela criada
         janela.getContentPane().add(campoTextoNome);
+        janela.getContentPane().add(botao);
 
         janela.setVisible(true); // exibindo ela na tela
     }
