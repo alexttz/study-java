3959a40b0230ccf6eecc1def2b96a5672ccb1c37
Alex Lima <lima.alexander.las@gmail.com>
2024-10-30 11:51:41 -0300

    Add new elements in Application and testing new elements in JFrame

--- main.java
+++ main.java
@@ -1,12 +1,25 @@
 import javax.swing.*;
 import javax.swing.JFrame;
-
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class main {
     public static void main(String[] args) {
-        JFrame janela = new JFrame();
+        JFrame janela = new JFrame(); //Criando uma janela.
         janela.setTitle("Meu primeiro programa");
         janela.setSize(500,500);
-        janela.setVisible(true);
+        janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //Metodo que encerra o programa quando clicamos no "X" na janela.
+
+        JLabel labelNome = new JLabel("Nome:"); // Criando uma label, inserindo um texto na janela
+
+        janela.setLayout(null); // removendo layouts da janela que vem por padrão
+        janela.getContentPane().add(labelNome); // adicionando a label na janela criada
+        labelNome.setBounds(20,20,50,20); // configurando a posião do texto (label)
+
+        JTextField campoTextoNome = new JTextField();
+        campoTextoNome.setBounds(20,40,200,20);
+        janela.getContentPane().add(campoTextoNome);
+
+        janela.setVisible(true); // exibindo ela na tela
     }
 }
